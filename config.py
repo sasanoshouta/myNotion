06dd9aab0c3dbcd84ddd117ee1c5500aae4a01c0
sasanoshouta <sasano.shouta.git@gmail.com>
2022-03-21 13:44:22 +0900

    fixed

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 class Config:
+    # 自身のトークンに変更
     notion_token = 'hogehoge'
     notion_endpoint = 'https://api.notion.com/v1'
+    # 更新したいDB_idに変更
     database_id = 'hogehoge'
     headers = {
         'Authorization': f'Bearer {notion_token}',
